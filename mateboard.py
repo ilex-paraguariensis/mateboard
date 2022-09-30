4a315d69bf4cb6a84fd4256910532650b6eaa182
GiulioZani <giulio.zani@gmail.com>
2022-09-30 20:16:07 +0200

    check if deno is installed

--- mateboard.py
+++ mateboard.py
@@ -8,7 +8,7 @@ class MateBoard:
 
     def start(self):
         try:
-            output = subprocess.check_output("deno", shell=True)                       
+            output = subprocess.check_output("deno" "-V", shell=True)                       
         except subprocess.CalledProcessError as grepexc:                                                                                                   
             print("Looks like you don't have deno installed")
         subprocess.Popen('deno task start'.split(), cwd=os.path.join(os.path.dirname(__file__), "mateboard"))
